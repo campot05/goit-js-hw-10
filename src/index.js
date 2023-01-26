9d462238a7e6cec3ab72360f3ec455dd5f0b18f6
campot05 <daniil.spectr@gmail.com>
2023-01-26 12:35:28 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,75 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
+import Debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
 const DEBOUNCE_DELAY = 300;
 
-fetchCountries('italy');
+const input = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+const clearItem = item => (item.innerHTML = '');
+
+input.addEventListener('input', Debounce(findCountries, 300));
+
+function findCountries() {
+  const value = input.value.trim();
+  if (!value) {
+    clearItem(listEl);
+    clearItem(infoEl);
+    return;
+  }
+
+  fetchCountries(value)
+    .then(data => {
+      if (data.length > 10) {
+        clearItem(listEl);
+
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      console.log('then');
+      markupRender(data);
+    })
+    .catch(err => {
+      Notify.failure('Oops, there is no country with that name');
+      return;
+    });
+}
+
+function markupRender(data) {
+  if (data.length === 1) {
+    clearItem(listEl);
+    clearItem(infoEl);
+    const item = createItem(data);
+    infoEl.insertAdjacentHTML('beforeend', item);
+  } else {
+    clearItem(listEl);
+    clearItem(infoEl);
+    const list = createList(data);
+    listEl.insertAdjacentHTML('beforeend', list);
+  }
+}
+
+function createItem(data) {
+  return data.map(({ name, capital, population, flags, languages }) => {
+    return `<h1><img src="${flags.svg}" alt="${
+      name.official
+    }" width='50' heigth='50'> ${name.common}</h1>
+		<p>Capital: ${capital[0]}</p>
+		<p>Population: ${population}</p>
+		<p>Languages: ${Object.values(languages)}</p>`;
+  });
+}
+
+function createList(data) {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.common}" width='45'>${name.common}</li>`
+    )
+    .join('');
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,5 @@
 export const fetchCountries = name => {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  ).then(response => response.json());
 };
