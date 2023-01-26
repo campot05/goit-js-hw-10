d3abf94bfaa3c18f243037b0d5112321a58dd319
campot05 <117174027+campot05@users.noreply.github.com>
2023-01-26 10:35:56 +0000

    Deploying to gh-pages from @ campot05/goit-js-hw-10@9d462238a7e6cec3ab72360f3ec455dd5f0b18f6 🚀

--- index.8a13bd19.js
+++
@@ -1,2 +0,0 @@
-fetch(`https://restcountries.com/v3.1/name/${"italy"}?fields=name,capital,population,flags,languages`).then((e=>e.json())).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.8a13bd19.js.map
